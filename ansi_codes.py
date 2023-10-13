cc10801ea986f3ab576787a76636f1605bf5f16c
danrog729 <130596844+danrog729@users.noreply.github.com>
2023-10-13 21:16:57 +0100

    ansi code class
    helpful for readability

---
+++ ansi_codes.py
@@ -0,0 +1,45 @@
+class txt:
+    class styles:
+        reset = "[0m"
+        bold = "[1m"
+        underline = "[4m"
+        inverse = "[7m"
+    class colours:
+        class foreground:
+            class normal:
+                black = "[30m"
+                red = "[31m"
+                green = "[32m"
+                yellow = "[33m"
+                blue = "[34m"
+                magnenta = "[35m"
+                cyan = "[36m"
+                white = "[37m"
+            class strong:
+                grey = "[90m"
+                red = "[91m"
+                green = "[92m"
+                yellow = "[93m"
+                blue = "[94m"
+                magnenta = "[95m"
+                cyan = "[96m"
+                white = "[97m"
+        class background:
+            class normal:
+                black = "[40m"
+                red = "[41m"
+                green = "[42m"
+                yellow = "[43m"
+                blue = "[44m"
+                magnenta = "[45m"
+                cyan = "[46m"
+                white = "[47m"
+            class strong:
+                grey = "[100m"
+                red = "[101m"
+                green = "[102m"
+                yellow = "[103m"
+                blue = "[104m"
+                magnenta = "[105m"
+                cyan = "[106m"
+                white = "[107m"
